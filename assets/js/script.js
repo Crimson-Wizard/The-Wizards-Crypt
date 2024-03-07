93d970578dd0acce13e503f6be19b977931e718e
Mike Lawlor <154077460+Crimson-Wizard@users.noreply.github.com>
2024-03-07 13:04:51 +0000

    add css style to h1 h2, hide game box till game start

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,16 @@ const pinTwo = document.getElementById('pin-two-input');
 const pinThree = document.getElementById('pin-three-input');
 const pinFour = document.getElementById('pin-four-input');
 const pinFive = document.getElementById('pin-five-input');
-let gameIntro = document.getElementById('game-intro');
+const gameBox = document.getElementById('game-box');
+const gameIntro = document.getElementById('game-intro');
+const gameArea = document.getElementById('game-area');
 let unlockButton = document.getElementById('unlock-button');
-let startButton = document.getElementById('start-game');
-let resetButton = document.getElementById('reset-game');
-let rulesButton = document.getElementById('game-rules');
+let startButton = document.getElementById('start');
+let resetButton = document.getElementById('reset');
 let instructionsDisplay = document.getElementById('instructions');
-let levelDisplay = document.getElementById('level-display');
-let scoreDisplay = document.getElementById('score-display');
-let lifeDisplay = document.getElementById('life-display');
+let levelDisplay = document.getElementById('level-display-counter');
+let scoreDisplay = document.getElementById('score-display-counter');
+let lifeDisplay = document.getElementById('life-display-counter');
 let gameOverDisplay = document.getElementById('game-over-display');
 let level1 = [pinOne, pinTwo, pinThree, pinFour, pinFive];
 let level2 = [pinTwo, pinOne, pinThree, pinFive, pinFour];
@@ -20,26 +21,18 @@ let level4 = [pinFour, pinThree, pinFive, pinOne, pinTwo];
 let level5 = [pinFive, pinFour, pinOne, pinTwo, pinThree];
 let level6 = [pinOne, pinThree, pinFour, pinTwo, pinFive];
 
+// function to hide menu and display game
+startButton.addEventListener('click', (startGame));
+
+function startGame() {
+    gameIntro.style.display = 'none';
+    gameArea.style.display = 'block';
+};
 
 //function to generate the sequence for each level
-/*function generateSequence(level) {
-    let sequence = [];
-    for (let i = 0; i < level.length; i++) {
-        sequence.push(level[i]);
-    }
-    return sequence;
-}
 
 //function to check if the user input matches the sequence
-function checksequence() {
-    let radiobuttons = document.querySelectorAll('.pin"]');
-    let correct = true;
 
-    radiobuttons.forEach(function (radiobutton, index) {
-        if (radiobutton.checked !== sequence[index]) {
-        correct = false;
-    }
-});
 //incomplete code!! reserach how to check if the user input matches the sequence
     if (correct) {
         alert('Correct, press unlock to move deep into the crypt!');
@@ -56,4 +49,3 @@ function checksequence() {
 //function to display the game over screen
 
 //function to display the win screen
-*/
