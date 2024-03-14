8e1c10f9f07daa797fd91165b3134ca3d3fecced
Mike Lawlor <154077460+Crimson-Wizard@users.noreply.github.com>
2024-03-14 13:13:28 +0000

    add js comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+/**
+ * This is the main variables for the game
+ */
 const pinOne = document.getElementById('pin-one-input');
 const pinTwo = document.getElementById('pin-two-input');
 const pinThree = document.getElementById('pin-three-input');
@@ -11,39 +14,55 @@ let unlockButton = document.getElementById('unlock');
 let startButton = document.getElementById('start');
 let resetButton = document.getElementById('reset-center');
 let clearButton = document.getElementById('clear');
+/**
+ * to display the instructions
+ */
 let instructionsDisplay = document.getElementById('instructions');
-let gameOverDisplay = document.getElementById('game-over-display');
-//to keep track of the life counter
+/**
+ * to display the life counter
+ */
 let wrongGuess = document.getElementById('wrong-guess-number');
 let currentGuess = parseInt(wrongGuess.innerHTML);
-//to keep track of the level counter
+/**
+ * to display the level counter
+ */
 let levelDisplay = document.getElementById('level-display-counter');
 let currentLevel = parseInt(levelDisplay.innerHTML);
-//to keep track of the score counter
+/**
+ * to display the score counter
+ */
 let scoreDisplay = document.getElementById('score-display-counter');
 let currentScore = parseInt(scoreDisplay.innerHTML);
+/**
+ * set the event listeners to false
+ */
 let listenersAttached = false;
+/**
+ * to display the message
+ */
 const messageElement = document.getElementById('message-box');
-
-
-// Ensure DOM content is loaded
-
-  
+/**
+ * variables for the game combinations and guesses
+ */
   const defaultCombo = [0, 1, 2, 3, 4];
   let guessCombo = [];
   let successCombo = []; 
   let correctGuesses = 0;
 
   console.log(pins);
-  // function to hide menu and display game
+  
+  /**
+   * ensure DOM content is loaded
+   * and add event listeners to the buttons
+   */
   document.addEventListener('DOMContentLoaded', function() {
-
       startButton.addEventListener('click', startGame);
       unlockButton.addEventListener('click', unlockDoor);
       clearButton.addEventListener('click', clearButtons);
-    
-
   });
+  /**
+   * function to activate the lock
+   */
   function activateLock() {
       pins.forEach(lockPin => {
           let clickHandler = function(event) {
@@ -54,7 +73,11 @@ const messageElement = document.getElementById('message-box');
           lockPin.addEventListener('click', clickHandler);
       });
   }
-
+/**
+ * function to start the game
+ * and hide the game intro
+ * and display the game area
+ */
   function startGame() {
       gameIntro.style.display = 'none';
       gameArea.style.display = 'block';
@@ -66,7 +89,9 @@ const messageElement = document.getElementById('message-box');
       console.log("levelCombo", successCombo);
       messageElement.textContent = '';
   }
-
+/**
+ * function to note the user guess
+ */
   function noteUserGuess(index) {
       addToGuessCombo(index);
       let isCorrect = index == successCombo[guessCombo.length - 1];
@@ -75,32 +100,31 @@ const messageElement = document.getElementById('message-box');
     if (index == successCombo[guessCombo.length - 1]) {
       updateLockDisplay(index, true);
     } else {
-
       updateLockDisplay(index,false);
-      
-    
-
     }
   }     
-
-  //function to deactivate event listeners
+  /**
+   * function to deactivate event listeners Prevent future attachment
+   */
   function activateLock() {
     if (!listenersAttached) {
         pins.forEach((lockPin, index) => {
             lockPin.addEventListener('click', () => noteUserGuess(index));
         });
-        listenersAttached = true; // Prevent future attachment
+        listenersAttached = true; 
     }
 }
-
-  
+  /**
+   * function to add the user guess to the guessCombo
+   */
   function addToGuessCombo(index) {
       console.log(index);
       guessCombo.push(index);
       console.log('guessCombo', guessCombo)
-
   }
-
+/**
+ * function to update the lock display
+ */
   function updateLockDisplay(index, isCorrect) {
     if(isCorrect) {
       pins[index].style.backgroundColor = 'green';
@@ -109,7 +133,6 @@ const messageElement = document.getElementById('message-box');
       pins[index].style.backgroundColor = 'red';
     }
   }
-
   // function to shuffle the array
   function shuffle(combination) {
       for (let i = combination.length - 1; i > 0; i--) {
@@ -118,41 +141,32 @@ const messageElement = document.getElementById('message-box');
       }
       return combination;
   }
-
-  
-
+  /**
+   * function to unlock the door
+   */
   function unlockDoor() {
     if (guessCombo.length < 5) {
       messageElement.textContent = 'You must select 5 pins';
       return;
-
     } else if (correctGuesses !== 5) {
           messageElement.textContent = 'Incorrect guess, Try again!';
           return;
       }
       if (guessCombo.toString() === successCombo.toString()) {
           messageElement.textContent = 'You have unlocked the door';
-
         currentLevel += 1;
         currentScore += 1;
-
-
         wrongGuess.innerHTML = currentGuess.toString();
         levelDisplay.innerHTML = currentLevel.toString();
         scoreDisplay.innerHTML = currentScore.toString();
-
         resetGame();
-
       } else {
-        
         resetGame();
       }
-      
   }
-
-
-  //function for clearing button
-  
+  /**
+   * function to clear the buttons
+   */
   function clearButtons() {
     guessCombo = [];
     messageElement.textContent = '';
@@ -161,13 +175,11 @@ const messageElement = document.getElementById('message-box');
     activateLock();
     pins.forEach(pin => {
       pin.style.backgroundColor = 'white';
-
   });
-  
-
 };
-
-
+/**
+ * function to reset the game
+ */
   function resetGame() {
       guessCombo = [];
       correctGuesses = 0;
@@ -177,27 +189,25 @@ const messageElement = document.getElementById('message-box');
       messageElement.textContent = '';
       startGame();
   }
-
 //
 /**
  * function to restart the game
  */
 resetButton.addEventListener('click', function() {
-  // Reset display settings
-  gameIntro.style.display = 'none'; // Show the game intro again if needed
-  gameArea.style.display = 'block'; // Hide the game area until start is clicked again
-  startButton.style.display = 'none'; // Show the start button to allow starting a new game
-  resetButton.style.display = 'block'; // Hide reset button until the game starts again
-  currentLevel = 1; // Reset level counter to initial value
-  currentScore = 0; // Reset score counter to initial value
-  currentGuess = 0; // Reset life counter to initial value
-  messageElement.textContent = ''; // Clear any messages
+  gameIntro.style.display = 'none'; 
+  gameArea.style.display = 'block';  
+  startButton.style.display = 'none';
+  resetButton.style.display = 'block'; 
+  currentLevel = 1; 
+  currentScore = 0; 
+  currentGuess = 0; 
+  messageElement.textContent = ''; 
 /** 
  * Restart counters
 */
-  wrongGuess.innerHTML = '0'; // Reset life counter to initial value
-  levelDisplay.innerHTML = '1'; // Reset level counter to initial value
-  scoreDisplay.innerHTML = '0'; // Reset score counter to initial value
+  wrongGuess.innerHTML = '0'; 
+  levelDisplay.innerHTML = '1'; 
+  scoreDisplay.innerHTML = '0'; 
 
   /** 
   * Reset the success combination to be set on new game start
@@ -210,7 +220,6 @@ resetButton.addEventListener('click', function() {
     pins.forEach(pin => {
     pin.style.backgroundColor = 'white'; 
   });
-
-  startGame(); // Start the game again
+  startGame(); 
 }); 
 
